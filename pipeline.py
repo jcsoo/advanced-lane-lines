a38d506de39b518e040d488d739cfea7ae6309f2
Jonathan Soo <jcsoo@agora.com>
2017-12-13 15:01:56 -0500

    Change perspective transform output

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import sys, os, pickle
 
 from calibrate import ImageLoader
@@ -59,8 +60,43 @@ class Pipeline:
         masked_image = cv2.bitwise_and(img, mask)
         return masked_image        
 
+    def mask_image_binary(self, img, mask):        
+        """
+        Applies an image mask.
+        
+        Only keeps the region of the image defined by the polygon
+        formed from `vertices`. The rest of the image is set to black.
+        """
+        if mask is None:
+            return img
+
+        imshape = img.shape
+
+        mask_w, mask_h = mask
+
+        vertices = np.array([[
+            (0,imshape[0]),
+            (imshape[1] * (0.5 - mask_w), imshape[0] * (1.0 - mask_h)), 
+            (imshape[1] * (0.5 + mask_w), imshape[0] * (1.0 - mask_h)), 
+            (imshape[1], imshape[0])
+        ]], dtype=np.int32)
+
+
+        #defining a blank mask to start with
+        mask = np.zeros_like(img)   
+        
+        #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+        ignore_mask_color = 1
+            
+        #filling pixels inside the polygon defined by "vertices" with the fill color    
+        cv2.fillPoly(mask, vertices, ignore_mask_color)
+        
+        #returning the image only where mask pixels are nonzero
+        masked_image = cv2.bitwise_and(img, mask)
+        return masked_image            
+
     def sobel(self, img):
-        k_size=7
+        k_size = 5
         sx_thresh = None #(20, 100), 
         sy_thresh = None #(0, 10), 
         mag_thresh = (50, 100)
@@ -127,6 +163,161 @@ class Pipeline:
         combined_binary[(s_binary == 1) | (sobel == 1)] = 1        
         return combined_binary
 
+    def find_lines(self, img):
+        ### From Class Notes
+
+        # Choose the number of sliding windows
+        nwindows = 9
+        # Set the width of the windows +/- margin
+        margin = 100
+        # Set minimum number of pixels found to recenter window
+        minpix = 20
+
+        binary_warped = img
+        # Find Lane Lines
+        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+
+        # Histogram of Warped Image
+        histogram = np.sum(img[img.shape[0]//2:,:], axis=0)
+        # Find midpoint
+        midpoint = np.int(histogram.shape[0]/2)
+        # Find left peak
+        leftx_base = np.argmax(histogram[:midpoint])
+        # Find right peak
+        rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        # Set height of windows
+        window_height = np.int(binary_warped.shape[0]/nwindows)
+        # Identify the x and y positions of all nonzero pixels in the image
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Current positions to be updated for each window
+        leftx_current = leftx_base
+        rightx_current = rightx_base
+
+        # Create empty lists to receive left and right lane pixel indices
+        left_lane_inds = []
+        right_lane_inds = []
+
+        # Step through the windows one by one
+        for window in range(nwindows):
+            # Identify window boundaries in x and y (and right and left)
+            win_y_low = binary_warped.shape[0] - (window+1)*window_height
+            win_y_high = binary_warped.shape[0] - window*window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+            # Draw the windows on the visualization image
+            cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),
+            (0,255,0), 2) 
+            cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),
+            (0,255,0), 2) 
+            # Identify the nonzero pixels in x and y within the window
+            good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+            (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
+            good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
+            (nonzerox >= win_xright_low) &  (nonzerox < win_xright_high)).nonzero()[0]
+            # Append these indices to the lists
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            # If you found > minpix pixels, recenter next window on their mean position
+            if len(good_left_inds) > minpix:
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if len(good_right_inds) > minpix:        
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+        # Concatenate the arrays of indices
+        left_lane_inds = np.concatenate(left_lane_inds)
+        right_lane_inds = np.concatenate(right_lane_inds)
+
+        # Extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds] 
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds] 
+
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+
+        # Generate x and y values for plotting
+        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+        plt.imshow(out_img)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)    
+        plt.show()
+        return (left_fit, right_fit, out_img)    
+
+    def find_lines_with_priors(self, img, left_fit, right_fit):        
+        binary_warped = img
+        # Assume you now have a new warped binary image 
+        # from the next frame of video (also called "binary_warped")
+        # It's now much easier to find line pixels!
+        nonzero = binary_warped.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        margin = 100
+        left_lane_inds = ((nonzerox > (left_fit[0]*(nonzeroy**2) + left_fit[1]*nonzeroy + 
+        left_fit[2] - margin)) & (nonzerox < (left_fit[0]*(nonzeroy**2) + 
+        left_fit[1]*nonzeroy + left_fit[2] + margin))) 
+
+        right_lane_inds = ((nonzerox > (right_fit[0]*(nonzeroy**2) + right_fit[1]*nonzeroy + 
+        right_fit[2] - margin)) & (nonzerox < (right_fit[0]*(nonzeroy**2) + 
+        right_fit[1]*nonzeroy + right_fit[2] + margin)))  
+
+        # Again, extract left and right line pixel positions
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds] 
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+        # Fit a second order polynomial to each
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+        # Generate x and y values for plotting
+        ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
+        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+        # Create an image to draw on and an image to show the selection window
+        out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+        window_img = np.zeros_like(out_img)
+        # Color in left and right line pixels
+        out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+        out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+        # Generate a polygon to illustrate the search window area
+        # And recast the x and y points into usable format for cv2.fillPoly()
+        left_line_window1 = np.array([np.transpose(np.vstack([left_fitx-margin, ploty]))])
+        left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx+margin, 
+                                    ploty])))])
+        left_line_pts = np.hstack((left_line_window1, left_line_window2))
+        right_line_window1 = np.array([np.transpose(np.vstack([right_fitx-margin, ploty]))])
+        right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx+margin, 
+                                    ploty])))])
+        right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+        # Draw the lane onto the warped blank image
+        cv2.fillPoly(window_img, np.int_([left_line_pts]), (0,255, 0))
+        cv2.fillPoly(window_img, np.int_([right_line_pts]), (0,255, 0))
+        result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+        plt.imshow(result)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0) 
+        plt.show()
+        return (left_fit, right_fit, out_img)    
+               
+
     def warp(self, img, M, img_size):
         return cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
 
@@ -148,21 +339,32 @@ class Pipeline:
         img_orig = img.copy()
 
         img = self.combined(img)
+        # img = self.sobel(img)
+        # img = self.color_threshold(img)
+        # img = self.mask_image_binary(img, self.cfg.get('mask', None))
+        img_masked = img.copy()
 
-        # img = self.mask_image(img, self.cfg.get('mask', None))
 
         # Center = 640, 420
         # [600, 450]
         src = np.array([(575, 450), (705, 450), (100, 680), (1180, 680)], np.float32)
-        dst = np.array([(0, 0), (400, 0), (0, 500), (400, 500)], np.float32)
+        dst = np.array([(0, 0), (1280, 0), (0, 720), (1280, 720)], np.float32)
 
         M = cv2.getPerspectiveTransform(src, dst)
 
-        #img = self.warp(img, M, (500, 500))
+        img = self.warp(img, M, (1280, 720))
+
+        (fitx, fity, out_img) = self.find_lines(img)
+        (fitx, fity, out_img) = self.find_lines_with_priors(img, fitx, fity)
+        # pipeline.view(out_img)
+        # self.display_lanes(img, fit)
+
+        # plt.plot(histogram)
+        # plt.show()
 
         #self.draw_text(img, 'Hello, World', (-500, 100), 2.0, WHITE)
         #self.draw_poly(img, [(0,0), (0, 100), (100, 100), (100, 0)], False, WHITE)
-        return img
+        return out_img
 
     def view(self, img):
         cv2.imshow('img', img)
