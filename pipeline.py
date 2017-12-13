79c413736a111fe61718239f3adcac293a9cf70c
Jonathan Soo <jcsoo@agora.com>
2017-12-13 15:37:28 -0500

    Add curve calculations

--- pipeline.py
+++ pipeline.py
@@ -406,6 +406,29 @@ class Pipeline:
         plt.show()        
         return (None, None, output)
 
+    def curve_radius_px(self, left_fit, right_fit, y_eval):
+        left_curverad = ((1 + (2*left_fit[0]*y_eval + left_fit[1])**2)**1.5) / np.absolute(2*left_fit[0])
+        right_curverad = ((1 + (2*right_fit[0]*y_eval + right_fit[1])**2)**1.5) / np.absolute(2*right_fit[0])
+        # Example values: 1926.74 1908.48
+        return left_curverad, right_curverad
+
+    def curve_radius_m(self, left_fit, right_fit, y_eval):
+        # Define conversions in x and y from pixels space to meters
+        ym_per_pix = 30/720 # meters per pixel in y dimension
+        xm_per_pix = 3.7/700 # meters per pixel in x dimension        
+
+        # # Fit new polynomials to x,y in world space
+        # left_fit_cr = np.polyfit(ploty*ym_per_pix, leftx*xm_per_pix, 2)
+        # right_fit_cr = np.polyfit(ploty*ym_per_pix, rightx*xm_per_pix, 2)
+
+        # # Calculate the new radii of curvature
+        # left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+        # right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+        # # Now our radius of curvature is in meters
+        # print(left_curverad, 'm', right_curverad, 'm')
+        # # Example values: 632.1 m    626.2 m
+        # return left_curverad, right_curverad
+
     def warp(self, img, M, img_size):
         return cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
 
@@ -458,6 +481,8 @@ class Pipeline:
         (fitx, fity, out_img) = self.find_lines(img)
         (fitx, fity, out_img) = self.find_lines_with_priors(img, fitx, fity)
 
+        (left_curve, right_curve) = self.curve_radius_px(fitx, fity, 720)
+        print(left_curve, right_curve)
         # pipeline.view(out_img)
         # self.display_lanes(img, fit)
 
